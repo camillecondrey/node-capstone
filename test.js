445bd091a4fb0512bd236bed7818568ee764ac15
Camille Condrey <camillecondrey@gmail.com>
2017-04-24 15:00:14 -0600

    fix test

--- test.js
+++ test.js
@@ -1,29 +1,22 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-// const uuid = require('uuid');
-const should = chai.should();
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('./server');
+
+var should = chai.should();
+var app = server.app;
+var storage = server.storage;
+
 chai.use(chaiHttp);
-const {app} = require('./server')
-
-	
-describe('assertions', function () {
-
-  it('#status property "status"', function () {
-    var res = { status: 200 };
-    res.should.to.have.status(200);
 
 
-it('#html', function () {
-    var req = { headers: { 'content-type': [ 'text/html' ] }};
-    var res = {
-      getHeader: function (key) {
-        return 'text/html'
-      }
-    };
-
-    req.should.be.html;
-    res.should.be.html;
-
-});
-});
+describe('index page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
 });
