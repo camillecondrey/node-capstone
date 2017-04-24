4ed2d66c064eadc3251680d1fc423d9ec43b2dde
Camille Condrey <camillecondrey@gmail.com>
2017-04-24 14:33:38 -0600

    second commit

--- server.js
+++ server.js
@@ -2,3 +2,25 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const uuid = require('uuid');
+// const should = chai.should();
+// chai.use(chaiHttp);
+
+// describe('GET endpoint', function() {
+// 	it('should return root url', function() {
+// 		let res;
+// 		return chai.request(app)
+// 			.get('../public/index.html')
+// 			.then(function(_res){
+// 				res = _res;
+// 				res.should.have.status(200);
+// 			});
+// 	});
+// });
